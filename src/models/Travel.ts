2c9931c03f9606708ae282d4bdfcf3573b1ab843
Lucas Matheus <lucasmgsilva@hotmail.com>
2021-12-31 13:36:32 -0300

    Reformulação do schema Travel

--- src/models/Travel.ts
+++ src/models/Travel.ts
@@ -1,17 +1,70 @@
 import { connection, Date, model, Schema } from "mongoose"
 
-type TravelType = {
-    trajeto_id: number
-    tempoInicio: Date,
-    tempoTermino: Date,
-    localizacao: [{lat: number, long: number}]
+type TrackingType = {
+    lat: number,
+    lng: number,
+    speed: number,
+    course: number
 }
 
+type TravelType = {
+    route_id: Schema.Types.ObjectId,
+    licensePlate_id: Schema.Types.ObjectId,
+    startTime: Date,
+    endTime?: Date,
+    isWayBack: boolean,
+    tracking: [TrackingType]
+}
+
+const TrackingSchema = new Schema<TrackingType>({
+    lat: {
+        type: Number,
+        required: true,
+        min: -90,
+        max: 90
+    },
+    lng: {
+        type: Number,
+        required: true,
+        min: -180,
+        max: 180
+    },
+    speed: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    course: {
+        type: Number,
+        required: true
+    }
+});
+
 const schema = new Schema<TravelType>({
-    trajeto_id: {type: Number, required: true},
-    tempoInicio: {type: Date, required: true},
-    tempoTermino: {type: Date},
-    localizacao: {type: [{lat: Number, long: Number}]}
+    route_id: {
+        type: Schema.Types.ObjectId, 
+        ref: 'Route', 
+        required: true
+    },
+    licensePlate_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Bus',
+        required: true
+    },
+    startTime: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    endTime: {
+        type: Date
+    },
+    isWayBack: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    tracking: [TrackingSchema]
 })
 
 const modelName: string = 'Travel';
