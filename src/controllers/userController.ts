ed68fea35ce3db5ee88378fe799c7c99e10cf37a
Gabriel Gomes da Silva <72311029+Bielgomes@users.noreply.github.com>
2022-09-08 15:30:33 -0300

    User Update do not change password if new password is empty

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -48,13 +48,15 @@ export const update = async (req: Request, res: Response) => {
 
         const user = await User.findById(_id);
         
-        if (user){
+        if (user) {
             user.name = name;
             user.email = email;
-            user.password = password;
             user.cellPhone = cellPhone;
             user.educationalInstitution = educationalInstitution;
             user.type = type;
+
+            if (password) user.password = password;
+
             await user.save();
             
             res.status(200).json(user);
